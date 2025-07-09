0d90a58f332b96ede0572d7fdf1b38135b93687b
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-07-10 03:43:32 +0530

    removing db pass from database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  //   await mongoose.connect(
-  //     "mongodb+srv://sguchhaitdev:Wdsr8RVAk85NPDC8@cluster0.q2agvgm.mongodb.net/devtinder"
-  //   );
-
   await mongoose.connect(process.env.DB_HOST);
 };
 
