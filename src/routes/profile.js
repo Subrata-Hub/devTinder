f55fae824fa64350a120bc13aec1a368e6047c17
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-07-12 01:22:30 +0530

    Implement logout & update profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const { userauth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const profileRoute = express.Router();
 
-profileRoute.get("/profile", userauth, async (req, res) => {
+profileRoute.get("/profile/view", userauth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
   } catch (error) {
     res.status(400).send("Error saving the user:" + error.message);
+  }
+});
+
+profileRoute.patch("/profile/edit", userauth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid edit request");
+    }
+
+    const logedInUser = req.user;
+    Object.keys(req.body).forEach((key) => (logedInUser[key] = req.body[key]));
+
+    await logedInUser.save();
+    res.json({
+      message: `${logedInUser.firstName}, your profile update successfully`,
+      data: logedInUser,
+    });
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
   }
 });
 
