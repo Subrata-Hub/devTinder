5e25f6646cf1b3fdc63db5e4146eb1d490c774e8
Subrata Guchhait <sguchhaitdev@gmail.com>
2025-07-12 18:07:12 +0530

    Creatingg connection request api && querying & index in DB and handle corner cases for connection request

--- apiList.md
+++ apiList.md
@@ -14,8 +14,8 @@
 
 ## ConnectionRequestRouter
 
-- POST /request/send/interested/:userId
-- POST /request/send/ignored/:userId
+- POST /request/send/:status/:userId
+<!-- - POST /request/send/ignored/:userId -->
 - POST /request/review/accepted/:requestId
 - POST /request/review/rejected/:requestId
 
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,12 +1,64 @@
 const express = require("express");
 const { userauth } = require("../middleware/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sentConnectionRequest", userauth, (req, res) => {
-  const user = req.user;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userauth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user?._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-  res.send(user.firstName + "sent conection request");
-});
+      const allowedStatus = ["ignored", "interested"];
+
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+
+      const toUser = await User.findById(toUserId);
+
+      if (!toUser) {
+        return res.status(404).json({
+          message: "User not found",
+        });
+      }
+
+      const existingConectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConectionRequest) {
+        return res.status(400).json({
+          message: "Connection request already exit",
+        });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: `${req.user.firstName} is ${status} in ${toUser.firstName}`,
+        data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR:" + error.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
